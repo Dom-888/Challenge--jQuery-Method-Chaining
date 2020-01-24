c8a66b6e5673e9df662de6264d3efe0b1d36c491
Dom-888 <spinelladomenico88@gmail.com>
2020-01-24 19:49:54 +0000

    Complete third challenge

---
+++ thirdChallenge.js
@@ -0,0 +1,8 @@
+//Add jQuery to the page’s DOM:
+
+var script = document.createElement('script');
+script.src = "//code.jquery.com/jquery-latest.min.js";
+document.head.appendChild(script);
+
+
+$("a").attr("href", "http://codeinstitute.net/")
