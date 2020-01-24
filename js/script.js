37a116f58e1f2c98da671b4209e355da82b96722
Dom-888 <spinelladomenico88@gmail.com>
2020-01-24 18:44:33 +0000

    Complete first challenge

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+ 
+    $("button").mouseenter(function(){
+        $(this).removeClass("makeRed").addClass("makeBorder");
+    });
+    $("button").mouseleave(function(){
+        $(this).removeClass("makeBorder").addClass("makeRed");
+    });
+    
+}); 
